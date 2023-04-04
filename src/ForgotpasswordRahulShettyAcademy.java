8811bebee9e1a6b4c343edb2d4eb5084901143c7
ashish4mahajan <55258145+ashish4mahajan@users.noreply.github.com>
2023-04-04 15:02:15 -0400

    remove comments

--- src/ForgotpasswordRahulShettyAcademy.java
+++ src/ForgotpasswordRahulShettyAcademy.java
@@ -47,8 +47,6 @@ public class ForgotpasswordRahulShettyAcademy {
 		driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
 		Thread.sleep(4000);
 	    driver.close();
-	    
-	    /// updated
 		
 	}
 
