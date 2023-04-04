28a91043fedc5e0d18bcf0d3ff8f48563a01cc02
ashish4mahajan <55258145+ashish4mahajan@users.noreply.github.com>
2023-04-04 14:53:30 -0400

    Create Sibling_Parent_Locator.java

---
+++ src/Sibling_Parent_Locator.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sibling_Parent_Locator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Users/mahaj/Downloads/chromedriver_win32 (1)/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+      // Relative Xpath
+		
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+		
+
+	}
+
+}
